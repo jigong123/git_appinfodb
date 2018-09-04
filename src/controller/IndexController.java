95587f86d9d9edf1c1b726ef5f2e5b14e743475d
1341130643 <Administrator@Z8E8X4RYDVP5UZR>
2018-09-04 08:38:46 +0800

    con

---
+++ src/controller/IndexController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Controller
+public class IndexController {
+	ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+	
+	
+}
